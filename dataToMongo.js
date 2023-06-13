4f918c66d676a9a03409ff06f5dd4d0a6056f132
Tyler G Alexander <tylergeorgealexander@gmail.com>
2023-06-13 13:07:18 -0400

    dataToMongo implemented to convert my JSON projects data to update to my-portfolio-projects

---
+++ dataToMongo.js
@@ -0,0 +1,24 @@
+const { MongoClient } = require('mongodb');
+const { projects } = require('./data');
+require('dotenv').config();
+
+const uri = process.env.MONGO_URI;
+const client = new MongoClient(uri);
+
+const insertProjectsData = async () => {
+    try {
+        await client.connect();
+
+        const database = client.db('my-portfolio-projects');
+        const collection = database.collection('projects');
+
+        const result = await collection.insertMany(projects);
+        console.log(`${result.insertedCount} documents inserted.`);
+    } catch (error) {
+        console.error('Error inserting data:', error);
+    } finally {
+        await client.close();
+    }
+};
+
+insertProjectsData();
